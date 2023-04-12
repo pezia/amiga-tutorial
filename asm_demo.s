5d7297ea3d5de74ee579a72f03ffd0d2f1e56081
Zsolt Petrik <pezia@pezia.hu>
2023-04-12 15:05:02 +0000

    Line bouncing

--- asm_demo.s
+++ asm_demo.s
@@ -7,17 +7,39 @@
 
 CIAA = $00bfe001
 
+init:
+    move       #$ac,d7                  ; start y position
+    move       #1,d6                    ; y add
 mainloop:
-    move.b #$ac,d0
+
+waitframe:
+    cmp.b      #$ff,CUSTOM+VHPOSR
+    bne        waitframe
+
+; ------ frame loop start ------
+    
+    add        d6,d7                    ; move y position
+
+    cmp        #$f0,d7                  ; bottom check
+    blo        ok1
+    neg        d6                       ; change direction
+ok1:
+    cmp        #$40,d7                  ; top check
+    bhi        ok2
+    neg        d6                       ; change direction
+ok2:
+
 waitras1:
-    cmp.b      #$ac,CUSTOM+VHPOSR
+    cmp.b      CUSTOM+VHPOSR,d7
     bne        waitras1
     move.w     #$fff,CUSTOM+COLOR00
 
 waitras2:
-    cmp.b      #$ac,CUSTOM+VHPOSR
+    cmp.b      CUSTOM+VHPOSR,d7
     beq        waitras2
     move.w     #$000,CUSTOM+COLOR00
+
+; ------ frame loop end ------
 
 .checkmouse:
     btst       #CIAB_GAMEPORT0,CIAA
