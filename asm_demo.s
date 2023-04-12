ccad2bed2fd8b029b10fa57617047462dbb9c013
Zsolt Petrik <pezia@pezia.hu>
2023-04-12 04:23:32 +0000

    Initial commit

---
+++ asm_demo.s
@@ -0,0 +1,13 @@
+;---------- Includes ----------
+              INCDIR      "include"
+              INCLUDE     "hw.i"
+              INCLUDE     "funcdef.i"
+              INCLUDE     "exec/exec_lib.i"
+              INCLUDE     "graphics/graphics_lib.i"
+              INCLUDE     "hardware/cia.i"
+;---------- Consts ----------
+
+CIAA            = $00bfe001
+
+ rts
+ 
